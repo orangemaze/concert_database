ae39ec037ecc7cbdb1412824e5f4447055cdc928
orangemaze <orangemaze@gmail.com>
2016-09-14 23:56:12 -0400

    geo_cities

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -15,4 +15,10 @@ module LocationHelper
     json = JSON.parse(open("http://ws.geonames.org/timezoneJSON?lat=#{lat_lon[0]}&lng=#{lat_lon[1]}&username=orangemaze", &:read))
   end
 
+  def get_cities_within_range(lat_lon, distance)
+    sql = "SELECT country, region, city, postalCode as postal_code, metroCode as metro_code, areaCode as area_code, ( 3959 * acos( cos( radians(#{lat_lon[0]}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(#{lat_lon[1]}) ) + sin( radians(#{lat_lon[0]}) ) * sin( radians( latitude ) ) ) ) AS geo_distance FROM geo_city where geo_city.activated = 'y' GROUP BY city HAVING geo_distance < #{distance} ORDER BY geo_distance"
+    get_cities_within_range = Geocity.find_by_sql(sql)
+  end
+
+
 end
